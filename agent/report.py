92b98da279229306d15ccd2fec3bb4e0ae2c385a
kqcoxn <mjy03730@163.com>
2025-10-24 22:37:38 +0800

    fix: 修复了无城市事件时可能判定为剧情事件并卡住的问题

--- agent/report.py
+++ agent/report.py
@@ -5,7 +5,7 @@ def punch_in():
     try:
         response = requests.post(
             "http://ts.codax.site/repo",
-            json={"from": "mnma", "version": "v3.0.0"},
+            json={"from": "mnma", "version": "v3.0.1"},
             headers={"Content-Type": "application/json"},
             timeout=3,
         )
