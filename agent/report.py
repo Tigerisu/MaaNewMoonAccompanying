4b30a1e7d76d791250d976be2d9397336acf5dc0
kqcoxn <mjy03730@163.com>
2025-11-02 22:00:16 +0800

    fix: 修复了战斗类事件有可能直接触发结束探索的问题

--- agent/report.py
+++ agent/report.py
@@ -5,7 +5,7 @@ def punch_in():
     try:
         response = requests.post(
             "http://ts.codax.site/repo",
-            json={"from": "mnma", "version": "v3.0.6"},
+            json={"from": "mnma", "version": "v3.0.7"},
             headers={"Content-Type": "application/json"},
             timeout=3,
         )
