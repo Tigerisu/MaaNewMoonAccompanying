053ce72e66196443d2ce598c0a2b35b6d7f0096a
kqcoxn <mjy03730@163.com>
2025-11-15 20:17:27 +0800

    chore: 撤回mfa版本

--- agent/report.py
+++ agent/report.py
@@ -5,7 +5,7 @@ def punch_in():
     try:
         response = requests.post(
             "http://ts.codax.site/repo",
-            json={"from": "mnma", "version": "v3.0.14"},
+            json={"from": "mnma", "version": "v3.0.15"},
             headers={"Content-Type": "application/json"},
             timeout=3,
         )
