7f963f6a3440ef6f81c5f06bc8f473708af7e1bb
kqcoxn <mjy03730@163.com>
2025-11-17 20:30:47 +0800

    fix: 补全倍率选择位置

--- agent/report.py
+++ agent/report.py
@@ -5,7 +5,7 @@ def punch_in():
     try:
         response = requests.post(
             "http://ts.codax.site/repo",
-            json={"from": "mnma", "version": "v3.0.19"},
+            json={"from": "mnma", "version": "v3.0.20"},
             headers={"Content-Type": "application/json"},
             timeout=3,
         )
